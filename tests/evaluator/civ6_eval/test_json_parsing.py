172c69a647709375089cc6f745f0454e750c034b
minsing-jin <ironman0722@naver.com>
2026-02-03 17:42:02 +0900

    Modify module name error

--- tests/evaluator/civ6_eval/test_json_parsing.py
+++ tests/evaluator/civ6_eval/test_json_parsing.py
@@ -3,7 +3,7 @@ import logging
 
 import pytest
 
-from computer_use_test.utils.provider.base import BaseVLMProvider
+from computer_use_test.utils.llm_provider.base import BaseVLMProvider
 
 # 테스트 시 로그 확인이 필요하다면 설정 (pytest -s 옵션으로 확인 가능)
 logger = logging.getLogger(__name__)
